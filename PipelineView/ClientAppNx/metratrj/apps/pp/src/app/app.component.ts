f1a2ad3bf2f7630fa14bbbf659472d2b5042ab65
Johannes Grimm <grimmjohannes1998@gmail.com>
2023-06-20 01:07:57 +0200

    try admin lte

--- PipelineView/ClientAppNx/metratrj/apps/pp/src/app/app.component.ts
+++ PipelineView/ClientAppNx/metratrj/apps/pp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { exampleProducts } from '@metratrj/products';
 
 @Component({
@@ -6,8 +7,35 @@ import { exampleProducts } from '@metratrj/products';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'pp';
+  loading = false;
+  initalLoadingComplete = false;
+
   products = exampleProducts;
   navigationProperies: any;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
+  ngAfterViewInit(): void {
+    this.initalLoadingComplete = true;
+  }
 }
