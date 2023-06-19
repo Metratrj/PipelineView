59a47487ea3c619c3f0e1a29ab6cdadf375d5bc4
Johannes Grimm <grimmjohannes1998@gmail.com>
2023-06-19 22:17:05 +0200

    Integrate navigation and view encapsulation

--- PipelineView/ClientAppNx/metratrj/libs/common-ui/src/lib/common-ui.module.ts
+++ PipelineView/ClientAppNx/metratrj/libs/common-ui/src/lib/common-ui.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule],
   declarations: [BannerComponent, NavigationComponent],
   exports: [BannerComponent, NavigationComponent],
 })
