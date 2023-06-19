0729374d4e8d4508b3cf399343b692285cd706cb
Johannes Grimm <grimmjohannes1998@gmail.com>
2023-06-19 21:23:33 +0200

    Add base structure for frontend

--- PipelineView/ClientAppNx/metratrj/apps/pp/src/app/app.component.ts
+++ PipelineView/ClientAppNx/metratrj/apps/pp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { exampleProducts } from '@metratrj/products';
 
 @Component({
   selector: 'metratrj-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pp';
+  products = exampleProducts;
 }
